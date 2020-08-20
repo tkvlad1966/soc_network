3564eae0966b0950003c3ef0783347364685299b
Vlad <tkvlads@gmail.com>
2020-08-20 19:27:40 +0300

    added 'logn'

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className={s.header}>
       <img src="https://t3.ftcdn.net/jpg/02/94/32/50/240_F_294325088_MEVHPiZe4mqXL2lBYiIWu5698mdYfv5s.jpg" />
-      <h2>FAMILY INFORMATION FIELD</h2>
+      <div className={s.login_block}>Login</div>
     </header>
   );
 };
