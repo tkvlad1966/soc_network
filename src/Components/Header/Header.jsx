bea393a211cf21a4c6f46979f3a2f60191143bf2
Vlad <tkvlads@gmail.com>
2020-08-21 11:01:06 +0300

    created HeaderContainer

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className={s.header}>
       <img src="https://t3.ftcdn.net/jpg/02/94/32/50/240_F_294325088_MEVHPiZe4mqXL2lBYiIWu5698mdYfv5s.jpg" />
-      <div className={s.login_block}>Login</div>
+      <div className={s.login_block}>
+        <NavLink to={'/login'}>Login</NavLink>
+      </div>
     </header>
   );
 };
