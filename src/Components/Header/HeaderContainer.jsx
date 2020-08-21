8edea5270300efb8e9a96be8a1de12b6f51a39b4
Vlad <tkvlads@gmail.com>
2020-08-21 14:14:03 +0300

    added dispath

--- src/Components/Header/HeaderContainer.jsx
+++ src/Components/Header/HeaderContainer.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import Header from './Header';
+import { getAuthorization } from '../common/api';
+import { connect } from 'react-redux';
+import { setAuthUserData } from '../../redux/auth-reducer';
 
 class HeaderContainer extends React.Component {
+  componentDidMount() {
+    getAuthorization().then((response) => {
+      if (response.data.resultCode === 0) {
+        let { id, login, email } = response.data.data;
+        this.props.setAuthUserData(id, login, email);
+      }
+    });
+  }
   render() {
-    return <Header />;
+    return <Header {...this.props} />;
   }
 }
 
-export default HeaderContainer;
+const mapStateToProps = (state) => ({
+  isAuth: state.auth.isAuth,
+  login: state.auth.login,
+});
+export default connect(mapStateToProps, { setAuthUserData })(HeaderContainer);
