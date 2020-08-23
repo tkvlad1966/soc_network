cc7316716408cd2a1fa5fe63d5a3e50bfa050b46
Vlad <tkvlads@gmail.com>
2020-08-23 14:22:40 +0300

    follov-unfollow add server

--- src/Components/common/api/index.js
+++ src/Components/common/api/index.js
@@ -5,6 +5,7 @@ export const getUsers = ({ page, count }) => {
     return axios
         .get(
             `${baseUrl}users?page=${page}&count=${count}`,
+            { withCredentials: true }
         )
 }
 export const getUserProfile = (userId) => {
@@ -18,5 +19,27 @@ export const getAuthorization = () => {
         .get(
             `${baseUrl}/auth/me`,
             { withCredentials: true, sameSite: 'none', secure: true }
+        )
+}
+export const postFollow = (userId) => {
+    return axios
+        .post(
+            `${baseUrl}follow/${userId}`, {}, {
+            withCredentials: true,
+            headers: {
+                'API-KEY': '49fcc7a6-93ef-4ac6-9181-4b2c76f5a553'
+            }
+        }
+        )
+}
+export const delFollow = (userId) => {
+    return axios
+        .delete(
+            `${baseUrl}follow/${userId}`, {
+            withCredentials: true,
+            headers: {
+                'API-KEY': '49fcc7a6-93ef-4ac6-9181-4b2c76f5a553'
+            }
+        }
         )
 }
