31d488fe440df1b65192baab0bd077058c3d0a4c
Vlad <tkvlads@gmail.com>
2020-08-23 14:20:57 +0300

    final modification added foto oflogin

--- src/Components/Header/HeaderContainer.jsx
+++ src/Components/Header/HeaderContainer.jsx
@@ -3,7 +3,6 @@ import Header from './Header';
 import { getAuthorization, getUserProfile } from '../common/api';
 import { connect } from 'react-redux';
 import { setAuthUserData, setUserPhotos } from '../../redux/auth-reducer';
-import { toogleIsFetching } from '../../redux/app-reducer';
 
 class HeaderContainer extends React.Component {
   componentDidMount() {
@@ -11,13 +10,10 @@ class HeaderContainer extends React.Component {
       if (response.data.resultCode === 0) {
         let { id, login, email } = response.data.data;
         this.props.setAuthUserData(id, login, email);
+        getUserProfile(id).then((response) => {
+          this.props.setUserPhotos(response.data.photos.small);
+        });
       }
-    });
-    let userId = this.props.userId;
-    this.props.toogleIsFetching(true);
-    getUserProfile(userId).then((response) => {
-      this.props.toogleIsFetching(false);
-      this.props.setUserPhotos(response.data.photos.small);
     });
   }
   render() {
@@ -29,13 +25,11 @@ const mapStateToProps = (state) => {
   return {
     isAuth: state.auth.isAuth,
     login: state.auth.login,
-    userId: state.auth.userId,
+    // userId: state.auth.userId,
     photos: state.auth.photos,
-    isFetching: state.app.isFetching,
   };
 };
 export default connect(mapStateToProps, {
   setAuthUserData,
   setUserPhotos,
-  toogleIsFetching,
 })(HeaderContainer);
