5d97e0dd96d6523cd38658ba26dd65eea58fbd5c
Vlad <tkvlads@gmail.com>
2020-08-27 16:15:16 +0300

    added getAuthUserData (thunk) vs auth-reduser

--- src/Components/Header/HeaderContainer.jsx
+++ src/Components/Header/HeaderContainer.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import Header from './Header';
-import { API } from '../common/api';
 import { connect } from 'react-redux';
-import { setAuthUserData, setUserPhotos } from '../../redux/auth-reducer';
+import { getAuthUserData } from '../../redux/auth-reducer';
 
 class HeaderContainer extends React.Component {
   componentDidMount() {
-    API.getAuthorization().then((response) => {
-      if (response.data.resultCode === 0) {
-        let { id, login, email } = response.data.data;
-        this.props.setAuthUserData(id, login, email);
-        API.getUserProfile(id).then((data) => {
-          this.props.setUserPhotos(data.photos.small);
-        });
-      }
-    });
+    this.props.getAuthUserData();
   }
   render() {
     return <Header {...this.props} />;
@@ -25,11 +16,7 @@ const mapStateToProps = (state) => {
   return {
     isAuth: state.auth.isAuth,
     login: state.auth.login,
-    // userId: state.auth.userId,
     photos: state.auth.photos,
   };
 };
-export default connect(mapStateToProps, {
-  setAuthUserData,
-  setUserPhotos,
-})(HeaderContainer);
+export default connect(mapStateToProps, { getAuthUserData })(HeaderContainer);
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,10 +1,9 @@
+import { API } from "../Components/common/api";
+
 const SET_USER_DATA = 'SET_USER_DATA';
 const SET_USER_PFOTOS = 'SET_USER_PFOTOS';
 
-export const setAuthUserData = (userId, login, email) => (
-    { type: SET_USER_DATA, data: { userId, login, email } });
-export const setUserPhotos = (photos) => (
-    { type: SET_USER_PFOTOS, photos });
+
 
 let initialState = {
     userId: null,
@@ -39,4 +38,22 @@ const authReducer = (state = initialState, action) => {
     }
 }
 
+export const setAuthUserData = (userId, login, email) => (
+    { type: SET_USER_DATA, data: { userId, login, email } });
+export const setUserPhotos = (photos) => (
+    { type: SET_USER_PFOTOS, photos });
+
+export const getAuthUserData = () => {
+    return (dispatch) => {
+        API.getAuthorization().then((response) => {
+            if (response.data.resultCode === 0) {
+                let { id, login, email } = response.data.data;
+                dispatch(setAuthUserData(id, login, email));
+                API.getUserProfile(id).then((data) => {
+                    dispatch(setUserPhotos(data.photos.small));
+                });
+            }
+        });
+    }
+}
 export default authReducer;
