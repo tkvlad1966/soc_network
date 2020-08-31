d5b96331dce67b1d1035987ab3ceb21214e9b4a3
Vlad <tkvlads@gmail.com>
2020-08-31 18:45:13 +0300

    work with local state, input, span

--- src/Components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/Components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -4,29 +4,40 @@ import React from 'react';
 class ProfileStatus extends React.Component {
   state = {
     editMode: false,
+    status: this.props.status,
+  };
+
+  OnStatusChange = (e) => {
+    const { value: status } = e.currentTarget;
+    this.setState({ status });
   };
   ActiveEditMode = () => {
     this.setState({ editMode: true });
   };
   deActiveEditMode = () => {
     this.setState({ editMode: false });
+    this.props.updateStatus(this.state.status);
   };
   render() {
     return (
       <div>
         {!this.state.editMode && (
-          <span onDoubleClick={this.ActiveEditMode}>{this.props.status}</span>
+          <span onDoubleClick={this.ActiveEditMode}>
+            {this.props.status || '_ _ _ _'}
+          </span>
         )}
         <div />
-        <div>
-          {this.state.editMode && (
+
+        {this.state.editMode && (
+          <div>
             <input
+              onChange={this.OnStatusChange}
               autoFocus={true}
               onBlur={this.deActiveEditMode}
-              value={this.props.status}
+              value={this.state.status}
             />
-          )}
-        </div>
+          </div>
+        )}
       </div>
     );
   }
