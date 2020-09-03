fada928e8230528cfb8a4d0ac52d01b469a3be0f
Vlad <tkvlads@gmail.com>
2020-09-03 16:40:46 +0300

    to apply validators for LoginForm

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form'
+import { maxLength, requiredField, minLength, passIsLetterAndNumbre } from '../../utils/validator';
+import { Input } from '../common/FormsControls';
+
+const maxLength10 = maxLength(10)
+const minLength6 = minLength(6)
 
 const Login = (props) => {
     const onSubmit = (formData) => {
@@ -17,10 +22,12 @@ const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={'Login'} name={'login'} component={'input'} />
+                <Field placeholder={'Login'} name={'login'} component={Input}
+                    validate={[requiredField, maxLength10, minLength6]} />
             </div>
             <div>
-                <Field placeholder={'Password'} name={'password'} component={'input'} />
+                <Field placeholder={'Password'} name={'password'} component={Input}
+                    validate={[requiredField, maxLength10, minLength6, passIsLetterAndNumbre]} />
             </div>
             <div>
                 <Field type={'checkbox'} name={'remember me'} component={'input'} /> remembre me
