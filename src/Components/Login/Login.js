763f2934a2d429db70b64df8fa32517ed8a9d02f
Vlad <tkvlads@gmail.com>
2020-09-04 18:27:28 +0300

    add to onSubmit: login rename Field

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form'
 import { maxLength, requiredField, minLength, passIsLetterAndNumbre } from '../../utils/validator';
 import { Input } from '../common/FormsControls';
+import { connect } from 'react-redux';
+import { login } from '../../redux/auth-reducer';
+import { Redirect } from 'react-router-dom';
 
-const maxLength10 = maxLength(10)
+const maxLength30 = maxLength(30)
 const minLength6 = minLength(6)
 
 const Login = (props) => {
+
     const onSubmit = (formData) => {
-        console.log(formData)
+        props.login(formData.email, formData.password, formData.remembreMe)
+    }
+    debugger
+    if (props.isAuth) {
+        return (
+            <Redirect to={'/profile'} />
+        )
     }
     return (
         <div>
@@ -22,21 +32,25 @@ const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={'Login'} name={'login'} component={Input}
-                    validate={[requiredField, maxLength10, minLength6]} />
+                <Field placeholder={'Email'} name={'email'} component={Input}
+                    validate={[requiredField, maxLength30, minLength6]} />
             </div>
             <div>
-                <Field placeholder={'Password'} name={'password'} component={Input}
-                    validate={[requiredField, maxLength10, minLength6, passIsLetterAndNumbre]} />
+                <Field placeholder={'Password'} name={'password'} component={Input} type={'password'}
+                    validate={[requiredField, maxLength30, minLength6, passIsLetterAndNumbre]} />
             </div>
             <div>
-                <Field type={'checkbox'} name={'remember me'} component={'input'} /> remembre me
+                <Field type={'checkbox'} name={'rememberMe'} component={'input'} /> remembre me
             </div>
             < button > Login</button >
         </form>
     )
 }
 
+let mapStateToProps = (state) => (
+    { isAuth: state.auth.isAuth }
+)
+
 const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
-export default Login
+export default connect(mapStateToProps, { login })(Login)
