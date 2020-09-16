3d73a17a0186d3fa0081e8b6c5fdc095c199f994
Vlad Tkachuk <tkvlads@gmail.com>
2020-09-16 12:36:28 +0300

    refactoring code

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import profileReducer from './profile-reducer';
 import dialogsReducer from './dialogs-reducer';
 import navbarReducer from './navbar-reducer';
@@ -6,20 +6,20 @@ import usersReducer from './users-reducer';
 import appReducer from './app-reducer';
 import authReducer from './auth-reducer';
 import thunkMiddleware from 'redux-thunk';
-import { reducer as formReducer } from 'redux-form'
+import { reducer as formReducer } from 'redux-form';
 
 let reducers = combineReducers({
-    profile: profileReducer,
-    dialogs: dialogsReducer,
-    navbar: navbarReducer,
-    usersPage: usersReducer,
-    app: appReducer,
-    auth: authReducer,
-    form: formReducer
-})
+  profile: profileReducer,
+  dialogs: dialogsReducer,
+  navbar: navbarReducer,
+  usersPage: usersReducer,
+  app: appReducer,
+  auth: authReducer,
+  form: formReducer,
+});
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
-console.log('store', store.getState());
+// console.log('store', store.getState());
 
 window.store = store;
 
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,6 +1,9 @@
-import { toogleIsFetching, toogleFollowingProgress, toogleUnFollowingProgress } from "./app-reducer";
+import {
+  toogleIsFetching,
+  toogleFollowingProgress,
+  toogleUnFollowingProgress,
+} from './app-reducer';
 import { UserAPI } from '../Components/common/api';
-
 
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
@@ -11,117 +14,113 @@ const SET_TOTAL_COUNT_USERS = 'SET_TOTAL_COUNT_USERS';
 export const follow = (usersid) => ({ type: FOLLOW, usersid });
 export const unfollow = (usersid) => ({ type: UNFOLLOW, usersid });
 export const setUsers = (users) => ({ type: SET_USERS, users });
-export const setCurrentPage = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage: currentPage });
-export const setTotalCountUsers = (totalCountUsers) => ({ type: SET_TOTAL_COUNT_USERS, totalCountUsers });
+export const setCurrentPage = (currentPage) => ({
+  type: SET_CURRENT_PAGE,
+  currentPage: currentPage,
+});
+export const setTotalCountUsers = (totalCountUsers) => ({
+  type: SET_TOTAL_COUNT_USERS,
+  totalCountUsers,
+});
 
 let initialState = {
-    users: [],
-    totalCountUsers: 0,
-    currentPage: 1,
-    sizePage: 6,
+  users: [],
+  totalCountUsers: 0,
+  currentPage: 1,
+  sizePage: 6,
 };
 
 const usersReducer = (state = initialState, action) => {
-    switch (action.type) {
-
-        case FOLLOW: {
-            return {
-                ...state,
-                users: state.users.map(u => {
-                    if (u.id === action.usersid) {
-                        return (
-                            { ...u, followed: true })
-                    }
-                    return u
-                })
-            }
-        }
-        case UNFOLLOW: {
-            return {
-                ...state,
-                users: state.users.map(u => {
-                    if (u.id === action.usersid) {
-                        return { ...u, followed: false }
-                    }
-                    return u
-                })
-            }
-        }
-        case SET_USERS: {
-
-            return {
-                ...state,
-                users: [...action.users],
-            }
-
-        }
-
-        case SET_CURRENT_PAGE: {
-
-            return {
-                ...state,
-                currentPage: action.currentPage
-            }
-        }
-        case SET_TOTAL_COUNT_USERS: {
-
-            return {
-                ...state,
-                totalCountUsers: action.totalCountUsers
-            }
-        }
-        default: return state;
+  switch (action.type) {
+    case FOLLOW: {
+      return {
+        ...state,
+        users: state.users.map((u) => {
+          if (u.id === action.usersid) {
+            return { ...u, followed: true };
+          }
+          return u;
+        }),
+      };
     }
-}
+    case UNFOLLOW: {
+      return {
+        ...state,
+        users: state.users.map((u) => {
+          if (u.id === action.usersid) {
+            return { ...u, followed: false };
+          }
+          return u;
+        }),
+      };
+    }
+    case SET_USERS: {
+      return {
+        ...state,
+        users: [...action.users],
+      };
+    }
+
+    case SET_CURRENT_PAGE: {
+      return {
+        ...state,
+        currentPage: action.currentPage,
+      };
+    }
+    case SET_TOTAL_COUNT_USERS: {
+      return {
+        ...state,
+        totalCountUsers: action.totalCountUsers,
+      };
+    }
+    default:
+      return state;
+  }
+};
 
 export const getUsersThunkCreator = (currentPage, sizePage) => {
-    return (dispatch) => {
-        dispatch(toogleIsFetching(true));
-        UserAPI.getUsers({
-            page: currentPage,
-            count: sizePage,
-        }).then((data) => {
-            dispatch(toogleIsFetching(false));
-            dispatch(setUsers(data.items));
-            dispatch(setTotalCountUsers(data.totalCount));
-        });
-    }
-}
+  return async (dispatch) => {
+    dispatch(toogleIsFetching(true));
+    const response = await UserAPI.getUsers({
+      page: currentPage,
+      count: sizePage,
+    });
+    dispatch(toogleIsFetching(false));
+    dispatch(setUsers(response.items));
+    dispatch(setTotalCountUsers(response.totalCount));
+  };
+};
 
 export const onClickFollowThunkCreator = (userId) => {
-    return async (dispatch) => {
-        dispatch(toogleFollowingProgress(userId));
-        try {
-            const response = await UserAPI.postFollow(userId);
-            if (response.data.resultCode === 0) {
-                dispatch(follow(userId));
-            }
-        }
-        catch (error) {
-            console.log('error', error);
-        }
-        finally {
-            dispatch(toogleUnFollowingProgress(userId));
-        }
-    };
-}
+  return async (dispatch) => {
+    dispatch(toogleFollowingProgress(userId));
+    try {
+      const response = await UserAPI.postFollow(userId);
+      if (response.data.resultCode === 0) {
+        dispatch(follow(userId));
+      }
+    } catch (error) {
+      console.log('error', error);
+    } finally {
+      dispatch(toogleUnFollowingProgress(userId));
+    }
+  };
+};
 
 export const onClickUnFollowThunkCreator = (userId) => {
-    return (dispatch) => {
-        dispatch(toogleFollowingProgress(userId));
-        UserAPI.delFollow(userId)
-            .then((response) => {
-                if (response.data.resultCode === 0) {
-                    dispatch(unfollow(userId));
-                }
-            })
-            .catch((error) => {
-                console.log('unfollow error', error);
-            })
-            .finally(() => {
-                dispatch(toogleUnFollowingProgress(userId));
-            });
-    };
-}
+  return async (dispatch) => {
+    dispatch(toogleFollowingProgress(userId));
+    try {
+      const response = await UserAPI.delFollow(userId);
+      if (response.data.resultCode === 0) {
+        dispatch(unfollow(userId));
+      }
+    } catch (error) {
+      console.log('unfollow error', error);
+    } finally {
+      dispatch(toogleUnFollowingProgress(userId));
+    }
+  };
+};
 
 export default usersReducer;
