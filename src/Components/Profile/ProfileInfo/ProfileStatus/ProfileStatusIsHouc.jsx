a1bc52b90a06a082e26b4fd2042b62cc5dacf904
Vlad Tkachuk <tkvlads@gmail.com>
2020-09-16 12:30:32 +0300

    added useEffect

--- src/Components/Profile/ProfileInfo/ProfileStatus/ProfileStatusIsHouc.jsx
+++ src/Components/Profile/ProfileInfo/ProfileStatus/ProfileStatusIsHouc.jsx
@@ -1,24 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import s from './ProfileInfo.module.css';
 
 const ProfileStatusIsHouc = (props) => {
   let [editMode, setEditMode] = useState(false);
   let [status, setStatus] = useState(props.status);
 
-  // useState = {
-  //   editMode: false,
-  //   status: props.status,
-  // };
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
 
   const OnStatusChange = (e) => {
     const { value: status } = e.currentTarget;
     setStatus(status);
-  };
-
-  const componentDidUpdate = (prevProps, prevState) => {
-    if (prevProps.status !== props.status) {
-      setStatus(props.status);
-    }
   };
 
   const ActiveEditMode = () => {
