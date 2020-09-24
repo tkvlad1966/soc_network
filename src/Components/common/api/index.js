0598908fc7a619d09a882db3c63ba3ae4925dd39
Vlad Tkachuk <tkvlads@gmail.com>
2020-09-24 15:55:20 +0300

    create ProfileApi.savePhoto

--- src/Components/common/api/index.js
+++ src/Components/common/api/index.js
@@ -1,63 +1,63 @@
-import * as axios from 'axios';
+import * as axios from "axios";
 
 const instanse = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    withCredentials: true,
-    headers: {
-        'API-KEY': 'b30605cc-6c34-4df2-95dd-3f0009408dc3'
-    }
-})
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  withCredentials: true,
+  headers: {
+    "API-KEY": "b30605cc-6c34-4df2-95dd-3f0009408dc3",
+  },
+});
 
 export const UserAPI = {
-    getUsers({ page = 1, count = 10 }) {
-        return instanse.get(`users?page=${page}&count=${count}`)
-            .then(response => {
-                return response.data
-            })
-    },
-    postFollow(userId) {
-        return instanse.post(
-            `follow/${userId}`, {}
-        )
-    },
-    delFollow(userId) {
-        return instanse.delete(
-            `follow/${userId}`
-        )
-    }
-}
+  getUsers({ page = 1, count = 10 }) {
+    return instanse
+      .get(`users?page=${page}&count=${count}`)
+      .then((response) => {
+        return response.data;
+      });
+  },
+  postFollow(userId) {
+    return instanse.post(`follow/${userId}`, {});
+  },
+  delFollow(userId) {
+    return instanse.delete(`follow/${userId}`);
+  },
+};
 
 export const ProfileAPI = {
-    getUserProfile(userId) {
-        return instanse.get(`profile/${userId}`)
-            .then(response => {
-                return response.data
-            })
-    },
-    updateStatus(status) {
-        return instanse.put(`profile/status`, { status: status })
-    },
-
-    getStatus(userId) {
-        return instanse.get(`profile/status/${userId}`)
-    },
-}
+  getUserProfile(userId) {
+    return instanse.get(`profile/${userId}`).then((response) => {
+      return response.data;
+    });
+  },
+  updateStatus(status) {
+    return instanse.put(`profile/status`, { status: status });
+  },
+  savePhoto(file) {
+    const formData = new FormData();
+    formData.append("image", file);
+    return instanse.put(`profile/photo`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  },
+  getStatus(userId) {
+    return instanse.get(`profile/status/${userId}`);
+  },
+};
 
 export const AuthAPI = {
-    getAuthorization() {
-        return instanse.get(`/auth/me`,
-            { sameSite: 'none', secure: true }
-        )
-    },
-    login(email, password, remembreMe) {
-        return instanse.post(`/auth/login`,
-            { email, password, remembreMe }
-        )
-    },
-    logout() {
-        return instanse.delete(`/auth/login`)
-    }
-}
+  getAuthorization() {
+    return instanse.get(`/auth/me`, { sameSite: "none", secure: true });
+  },
+  login(email, password, remembreMe) {
+    return instanse.post(`/auth/login`, { email, password, remembreMe });
+  },
+  logout() {
+    return instanse.delete(`/auth/login`);
+  },
+};
 
 // export const getUsers = ({ page = 1, count = 10 }) => {
 //     return instanse.get(`users?page=${page}&count=${count}`)
