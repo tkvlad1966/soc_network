9697c6f0d1f46f90209056be6790869046a65cce
Vlad Tkachuk <tkvlads@gmail.com>
2020-09-28 16:04:10 +0300

    created ProfileDataForm

---
+++ src/Components/Profile/ProfileInfo/ProfileDataForm.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { reduxForm } from "redux-form";
+import { createFielde, Input, Textarea } from "../../common/FormsControls";
+import { Contact } from "./ProfileInfo";
+
+const ProfileDataForm = ({ handleSubmit, profile }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <button onClick={() => {}}>save</button>
+      </div>
+      <div>
+        <b>Full name:</b>
+        {createFielde("Full name", "fullName", [], Input)}
+      </div>
+      <div>
+        <b>Looking for a job:</b>
+        {createFielde("", "lookingForAJob", [], Input, { type: "checkbox" })}
+      </div>
+      <div>
+        <b>My professional skills:</b>
+        {createFielde(
+          "My professional skills",
+          "lookingForAJobDescription",
+          [],
+          Textarea
+        )}
+      </div>
+      <div>
+        <b>About Me:</b>
+        {createFielde("About Me", "aboutMe", [], Textarea)}
+      </div>
+      <div>
+        <b>Contacts:</b>
+        {Object.keys(profile.contacts).map((key) => {
+          return (
+            <div key={key}>
+              <b>
+                {key}: {createFielde(key, "contacts." + key, [], Input)}
+              </b>
+            </div>
+          );
+        })}
+      </div>
+    </form>
+  );
+};
+
+const ProfileDataFormReduxForm = reduxForm({ form: "edit-profile" })(
+  ProfileDataForm
+);
+
+export default ProfileDataFormReduxForm;
