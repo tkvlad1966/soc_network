68b112c1a9a52e610b15ddcc11bfd34625f161ce
Vlad Tkachuk <tkvlads@gmail.com>
2020-09-30 09:21:11 +0300

    added Captcha logic

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,8 +1,9 @@
-import { AuthAPI, ProfileAPI } from "../Components/common/api";
+import { AuthAPI, ProfileAPI, securityAPI } from "../Components/common/api";
 import { stopSubmit } from "redux-form";
 
 const SET_USER_DATA = "SET_USER_DATA";
 const SET_USER_PFOTOS = "SET_USER_PFOTOS";
+const GET_CAPTCHA_URL_SUCCESS = "samurai-network/auth/GET_CAPTCHA_URL_SUCCESS";
 
 let initialState = {
   userId: null,
@@ -10,16 +11,18 @@ let initialState = {
   email: null,
   photos: null,
   isAuth: false,
+  captchaUrl: null,
 };
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SET_USER_DATA: {
+    case SET_USER_DATA:
+    case GET_CAPTCHA_URL_SUCCESS:
       return {
         ...state,
-        ...action.data,
+        ...action.payload,
       };
-    }
+
     case SET_USER_PFOTOS: {
       return {
         ...state,
@@ -33,7 +36,11 @@ const authReducer = (state = initialState, action) => {
 
 export const setAuthUserData = (userId, login, email, isAuth) => ({
   type: SET_USER_DATA,
-  data: { userId, login, email, isAuth },
+  payload: { userId, login, email, isAuth },
+});
+export const getCaptchaUrlSuccess = (captchaUrl) => ({
+  type: GET_CAPTCHA_URL_SUCCESS,
+  payload: { captchaUrl },
 });
 export const setUserPhotos = (photos) => ({ type: SET_USER_PFOTOS, photos });
 
@@ -50,12 +57,15 @@ export const getAuthUserData = () => {
   };
 };
 
-export const login = (email, password, remembreMe) => {
+export const login = (email, password, remembreMe, captcha) => {
   return async (dispatch) => {
-    const response = await AuthAPI.login(email, password, remembreMe);
+    const response = await AuthAPI.login(email, password, remembreMe, captcha);
     if (response.data.resultCode === 0) {
       dispatch(getAuthUserData());
     } else {
+      if (response.data.resultCode === 10) {
+        dispatch(getCaptchaUrl());
+      }
       let message =
         response.data.messages.length > 0
           ? response.data.messages
@@ -64,6 +74,13 @@ export const login = (email, password, remembreMe) => {
     }
   };
 };
+
+export const getCaptchaUrl = () => async (dispatch) => {
+  const response = await securityAPI.getCaptchaUrl();
+  const captchaUrl = response.data.url;
+  dispatch(getCaptchaUrlSuccess(captchaUrl));
+};
+
 export const logout = () => {
   return async (dispatch) => {
     const response = await AuthAPI.logout();
