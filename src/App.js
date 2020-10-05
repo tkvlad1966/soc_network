ae3a17739f56331e6a5b628497e96e1f57948f81
Vlad Tkachuk <tkvlads@gmail.com>
2020-10-05 08:07:48 +0300

    nothing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
-import Music from './Components/Music/Music';
-import { BrowserRouter, withRouter, Route } from 'react-router-dom';
-import News from './Components/News/News';
-import Setting from './Components/Setting/Setting';
-import NavBarContainer from './Components/NavBar/NavBarContainer';
-import DialogsContainer from './Components/Dialogs/DialogsContainer';
-import UsersContainer from './Components/Users/UsersContainer';
-import ProfileContainer from './Components/Profile/ProfileContainer';
-import HeaderContainer from './Components/Header/HeaderContainer';
-import Login from './Components/Login/Login';
-import { initializeApp } from './redux/app-reducer';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import Spinner from './Components/common/spinner';
+import React, { Component } from "react";
+import "./App.css";
+import Music from "./Components/Music/Music";
+import { withRouter, Route } from "react-router-dom";
+import News from "./Components/News/News";
+import Setting from "./Components/Setting/Setting";
+import NavBarContainer from "./Components/NavBar/NavBarContainer";
+import DialogsContainer from "./Components/Dialogs/DialogsContainer";
+import UsersContainer from "./Components/Users/UsersContainer";
+import ProfileContainer from "./Components/Profile/ProfileContainer";
+import HeaderContainer from "./Components/Header/HeaderContainer";
+import Login from "./Components/Login/Login";
+import { initializeApp } from "./redux/app-reducer";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import Spinner from "./Components/common/spinner";
 
 class App extends Component {
   componentDidMount() {
@@ -43,5 +43,5 @@ const mapStateToProps = (state) => ({ initialized: state.app.initialized });
 
 export default compose(
   withRouter,
-  connect(mapStateToProps, { initializeApp }),
+  connect(mapStateToProps, { initializeApp })
 )(App);
